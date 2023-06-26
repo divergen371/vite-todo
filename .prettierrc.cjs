26daf1a07b62576ca2836d50933590599c1f8be4
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-27 01:25:23 +0900

    first commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  allowParens: 'avoid',
+  bracketSameLine: true,
+  htmlWhitespaceSensitivity: 'ignore',
+}

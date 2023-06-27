a3436d5cf6efa3c31cd6c5f39618da705239527d
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-27 11:55:59 +0900

    :alembic: ボタンが押されたときのイベントを設定。ローカルストレージを使ってTODOを保存

--- src/components/MainTodo.vue
+++ src/components/MainTodo.vue
@@ -1,12 +1,16 @@
 <script setup>
 import { ref } from 'vue'
 const todoRef = ref('')
+
+const addTodo = () => {
+  localStorage.todoList = todoRef.value
+}
 </script>
 
 <template>
   <div class="box_input">
-    <input type="text" name="" id="" class="todo_input" v-model="todoRef" placeholder="+ TODOを入力" />
-    <button class="btn">追加</button>
+    <input type="text" class="todo_input" v-model="todoRef" placeholder="+ TODOを入力" />
+    <button class="btn" @click="addTodo">追加</button>
   </div>
 </template>
 
